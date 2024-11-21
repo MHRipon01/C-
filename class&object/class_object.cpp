248096a2e8b7396db379c6e2f11f8c7a505dfe75
MHRipon01 <136681772+MHRipon01@users.noreply.github.com>
2024-11-22 00:55:50 +0600

    class object comple

---
+++ class&object/class_object.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    char name[100];
+    int roll;
+    double gpa;
+};
+
+int main()
+{
+    Student a;
+    Student b;
+
+    // a.roll = 10; //static value
+    // a.gpa = 3.4; //static value
+    // a.name = "Ripon"; //static value
+    cin.getline(a.name, 100);
+    cin >> a.roll >> a.gpa;
+
+    cin.ignore();
+    cin.getline(b.name, 100);
+    cin >> b.roll >> b.gpa;
+    cout << a.name << " " << a.roll << " " << a.gpa << endl;
+    cout << b.name << " " << b.roll << " " << b.gpa << endl;
+
+    return 0;
+}
---
+++ class&object/string.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char name[100] = "Mh Ripon";
+    char name[20];
+    char temp[100] = "" name = "ripon"; // not possible as it's ultimately an array.
+    return 0;
+}
