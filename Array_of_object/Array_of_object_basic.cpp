cdce7138a1970987af6cd82980b50b205df50de6
MHRipon01 <136681772+MHRipon01@users.noreply.github.com>
2024-12-03 02:02:29 +0600

    array of object basic done

---
+++ Array_of_object/Array_of_object_basic.cpp
@@ -0,0 +1,67 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Student
+// {
+// public:
+//     string name;
+//     int roll;
+//     int marks;
+// };
+
+// int main()
+// {
+//     // int a[5];
+//     // Student rakib;
+
+//     int n;
+//     cin >> n;
+//     Student a[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin.ignore();
+//         getline(cin, a[i].name);
+
+//         cin >> a[i].roll >> a[i].marks;
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+//     }
+
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int cls;
+    int marks;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    Student a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin.ignore();
+        getline(cin,a[i].name);
+        cin >> a[i].cls >> a[i].marks;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i].name << " " << a[i].cls << " " << a[i].marks << endl;
+    }
+
+    return 0;
+}
